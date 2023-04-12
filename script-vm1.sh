b69e0d06a69058d15675e72b0f156e627066c537
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-04-12 09:48:44 +0200

    Update script-vm1.sh

--- script-vm1.sh
+++ script-vm1.sh
@@ -47,7 +47,7 @@ mysql --user=NomDeConnexionDeLAdministrateurDuServeur --password=MotDePasse --ho
 sudo cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
 sudo sed -i "s/database_name_here/wordpress/" /var/www/html/wordpress/wp-config.php
-sudo sed -i "s/username_here/NomDeConnexionDeLAdministrateurDuServeur7/" /var/www/html/wordpress/wp-config.php
+sudo sed -i "s/username_here/NomDeConnexionDeLAdministrateurDuServeur/" /var/www/html/wordpress/wp-config.php
 sudo sed -i "s/password_here/MotDePasse/" /var/www/html/wordpress/wp-config.php
 sudo sed -i "s/localhost/NomDuServeur/" /var/www/html/wordpress/wp-config.php
 sudo sed -i "s/It works!/VM1/" /var/www/html/index.html
